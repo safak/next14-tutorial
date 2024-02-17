c5914a7d929e8e98fc0de295f2e3910956f2ca16
luke <1156953217@qq.com>
2024-02-17 15:07:10 +0800

    from scratch

--- src/app/error.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-const Error = () => {
-  return (
-    <div>Error</div>
-  )
-}
-
-export default Error
--- src/app/loading.jsx
+++
@@ -1,7 +0,0 @@
-const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
-
-export default Loading
